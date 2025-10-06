74d67a68806b81b18b309c54a97ebe6d9a81f728
Vivek Ranjan <vvivekranjan20@gmail.com>
2025-10-06 21:16:09 +0530

    file name changed

--- goal_tracker/lib/profile/profile_screen.dart
+++ goal_tracker/lib/profile/profile_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:goal_tracker/change_password.dart';
+import 'package:goal_tracker/validate_password.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
